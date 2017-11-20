3fcfa1954793eb78284f270bdee42dc4a27989ba
towakey <kyosgame@gmail.com>
2017-11-21 00:21:19 +0900

    スタートページの作成1

---
+++ include/start/setup1.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Language Check</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
