3d4a9f118448132bfee5a860e5c2a49b0120266c
towakey <kyosgame@gmail.com>
2017-11-21 00:59:52 +0900

    setup1 ->add setup2 ->add setup.css ->add

---
+++ include/start/step1.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Language Check</title>
+    </head>
+    <body>
+        <div class="step">Step1</div>
+        <div class="title-en">Language Check</div>
+        <div class="title-ja">言語選択</div>
+    </body>
+</html>
