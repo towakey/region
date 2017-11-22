6563adc3f722b1af47ddfd9edc14697d54eb78fc
towakey <kyosgame@gmail.com>
2017-11-23 01:56:37 +0900

    setting.ini追加 setup2.php setup.css 更新

--- include/function.php
+++ include/function.php
@@ -1,8 +1,17 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 
-if(!is_readable("./setting.ini")){
-    $err="Setting fils is Nothing";
+$path=dirname(__FILE__)."/";
+if(!is_readable($path."setting.ini")){
+    $err="Setting fils is Not Found";
     echo $err;
 }
+$ini_path=$path."setting.ini";
+$ini=parse_ini_file($ini_path,true);
+
+$sql_user=$ini["sql_setting"]["user"];
+$sql_pass=$ini["sql_setting"]["pass"];
+
+print $sql_pass;
+
 ?>
--- include/start/step2.php
+++ include/start/step2.php
@@ -6,9 +6,17 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="setup.css">
+        <title>Language Check</title>
     </head>
     <body>
-        
+        <div id="wrapper">
+            <div class="step">Step2</div>
+            <div class="step_title">SQLの設定</div>
+            <div class="input_date"><div class="items">HostName</div><div class="inputs"><input type="text"></div></div>
+            <div class="input_date"><div class="items">UserName</div><div class="inputs"><input type="text"></div></div>
+            <div class="input_date"><div class="items">Password</div><div class="inputs"><input type="text"></div></div>
+            <div class="input_date"><div class="items">DateBase</div><div class="inputs"><input type="text"></div></div>
+        </div>
     </body>
 </html>
