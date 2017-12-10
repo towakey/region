6f531611b27ac68bed8f9da4be5b28a3a564b51f
towakey <kyosgame@gmail.com>
2017-12-10 18:54:17 +0900

    loginページ作成 loginチェッククラス作成

--- include/function.php
+++ include/function.php
@@ -3,14 +3,55 @@ header("Content-type: text/html; charset=utf-8");
 
 $path=dirname(__FILE__)."/";
 if(!is_readable($path."setting.ini")){
-    $err="Setting fils is Not Found";
-    echo $err;
+    http_response_code(301);
+    header("Location:../../login.php");
+    exit;
 }
-$ini_path=$path."setting.ini";
-$ini=parse_ini_file($ini_path,true);
 
-$sql_user=$ini["sql_setting"]["user"];
-$sql_pass=$ini["sql_setting"]["pass"];
+class login_check{
+    private $mysqli;
+
+    function __construct(){
+        $path=dirname(__FILE__);
+        $ini_path=$path."/setting.ini";
+
+        $ini=parse_ini_file($ini_path,true);
+
+        $hostname=$ini["sql_setting"]["hostname"];
+        $username=$ini["sql_setting"]["username"];
+        $password=$ini["sql_setting"]["password"];
+        $datebase=$ini["sql_setting"]["datebase"];
+
+        $this->mysqli=new mysqli($hostname,$username,$password,$datebase);
+        $this->mysqli->set_charset("utf8");
+    }
+    function check($input_user,$input_pass){
+        $sql="SELECT * FROM personalinfo WHERE item = 'username' OR item = 'password'";
+        if($result=$this->mysqli->query($sql)){
+            while($row=$result->fetch_assoc()){
+                switch($row["item"]){
+                    case "username":
+                    $user=$row["contents"];
+                    break;
+                    case "password":
+                    $pass=$row["contents"];
+                    break;
+                }
+            }
+            $result->close();
+            if($user === $input_user && $pass === $input_pass){
+                return TRUE;
+            }else{
+                return FALSE;
+            }
+        }else{
+            return FALSE;
+        }
+    }
+    function close(){
+        $this->mysqli->close();
+    }
+}
 
 
 ?>
--- login.php
+++ login.php
@@ -1,6 +1,21 @@
 <?php
-    http_response_code(301);
-    //header("Location:include/start/step2.php");
+require_once(realpath("./")."/include/function.php");
+
+if(isset($_POST["submit"])){
+    $user=htmlspecialchars($_POST["username"],ENT_QUOTES);
+    $pass=htmlspecialchars($_POST["password"],ENT_QUOTES);
+    if(is_readable(realpath("./include")."/setting.ini")){
+        $login_check=new login_check();
+        if($login_check->check($user,$pass)){
+            http_response_code(301);
+            header("Location:./index.php");
+            exit;
+        }else{
+        }
+        $login_check->close();
+    }else{
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,10 +28,11 @@
     <body>
         <div id="wrapper">
             <div id="login_box">
-                <form action="<?=$_SERVER["PHP_SELF"]?>" method="post">
-                    <div class="input_block"><div class="item">ユーザー名</div><div class="contents"><input type="text" name="" value="" size="30"></div></div>
-                    <div class="input_block"><div class="item">パスワード</div><div class="contents"><input type="text" name="" value="" size="30"></div></div>
-                    <div class="input_block"><button id="login_button">ログイン</button></div>
+                <form action="<?=$_SERVER["PHP_SELF"]?>" method="POST">
+                    <div class="title_block"><div class="title_contents">ユーザー名とパスワードの入力</div></div>
+                    <div class="input_block"><div class="item">ユーザー名</div><div class="contents"><input type="text" name="username" value="" size="30"></div></div>
+                    <div class="input_block"><div class="item">パスワード</div><div class="contents"><input type="password" name="password" value="" size="30"></div></div>
+                    <div class="input_block"><button type="submit" name="submit" id="login_button">ログイン</button></div>
                 </form>
             </div>
         </div>
